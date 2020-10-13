835f8c7b7eb68dce36fde9eda864c8307f18b634
Adabelle <adita_citlali@hotmail.com>
2020-10-13 17:55:30 -0500

    es mi tabla de frecuencia de iris, lo hice junto a Yaksi:)

---
+++ iris.R
@@ -0,0 +1,34 @@
+data(iris)
+
+names(iris)
+
+dim(iris)
+
+str(iris)
+
+anyNA(iris)
+
+iris$Species
+
+typeof(iris)
+
+tabla_PL<-as.data.frame(table(PL = iris$Petal.Length))
+
+tabla_PL
+
+transform(tabla_PL,
+          freqAc=cumsum(Freq),
+          Rel= round(prop.table(Freq),3),
+          RelAc=round(cumsum(prop.table(Freq)),3))
+
+tabla_clases<-as.data.frame(table
+                            (Petal.length = factor
+                              (cut(iris$Petal.Length,
+                                   breaks = 9))))
+
+tabla_clases                        
+
+transform(tabla_clases,
+          freqAc=cumsum(Freq),
+          Rel= round(prop.table(Freq),3),
+          RelAc=round(cumsum(prop.table(Freq)),3))
